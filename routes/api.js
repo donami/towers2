d176d809fea37d318dcd7e725712aba968223949
markus <d0namiz@gmail.com>
2016-12-15 11:06:39 +0100

    Players are inserted automatically in the database

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,27 @@ var leaderboardRoutes = require('./leaderboard');
 var hallOfFameRoutes = require('./halloffame');
 var newMoonsRoutes = require('./newMoons');
 
+function checkIfPlayerExistsInDB(data) {
+    db.query('SELECT * FROM Player WHERE id = ?',
+      [ data.playerId ],
+      function(err, rows) {
+        if (err)
+          throw err;
+
+        // If player doesn not already exist in database, insert player row
+        if (!rows[0]) {
+          db.query(
+            'INSERT INTO Player (id, alias) VALUES (:playerId, :alias)',
+            {playerId: data.playerId, alias: data.alias},
+            function(err, result) {
+              if (err)
+                throw err;
+            }
+          );
+        }
+      });
+}
+
 router.get('/verify-key/:key', function(req, res) {
   var options = {
     uri: api.API_ME + '?apiKey=' + req.params.key,
@@ -19,6 +40,7 @@ router.get('/verify-key/:key', function(req, res) {
 
   request(options)
     .then(function(data) {
+      checkIfPlayerExistsInDB(data);
       return res.json(data);
     }, function(error) {
       return res.status(403).send('Invalid credentials');
